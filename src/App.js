864e631410e79928ef07121c11d95a04b07f5fd9
Yusuf Said SAYIN <yusuff6752@gmail.com>
2022-11-29 01:09:20 +0300

    page title was made for each page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from './Components/Header'
+import Pricing from './Pages/Pricing'
 const App = () => {
   return (
     <div className=''>
       <Header></Header>
+      <Pricing></Pricing>
     </div>
   )
 }
---
+++ src/Pages/Pricing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageTitle from '../Components/PageTitle'
+
+const Pricing = () => {
+  return (
+    <>
+    <PageTitle title="Pricing Plans" text="We are an industry-leading company that values honesty, integrity, and efficiency. Building quality products and caring."></PageTitle>
+    </>
+  )
+}
+
+export default Pricing
